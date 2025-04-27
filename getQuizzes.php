8b0b8c621b313d47b31f9faaba64cda1e258ccae
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-04-27 06:24:03 +0300

    creating <> creating get quizzes API

---
+++ getQuizzes.php
@@ -0,0 +1,24 @@
+<?php 
+include('./connection.php');
+
+header('Content-Type: application/json');
+
+try {
+
+
+        $query = $connection->prepare("SELECT * FROM `quizzes` ");
+        $query->execute();
+
+
+ $result=[];
+ while ($quiz=$query->fetch(PDO::FETCH_ASSOC)){
+//  array_push(result,$quiz);
+ $result[]=$quiz;
+ }
+echo json_encode($result);
+}
+catch (Throwable $e) {
+    echo json_encode([
+        "message" => "error",
+    ]);
+}
